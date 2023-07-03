4fca132c706bbd9d00347fec581e284970aa1d3e
Alex Peterson <aspeterson.126@gmail.com>
2023-07-03 17:19:49 -0500

    Add createCard function to return card object

---
+++ src/card.js
@@ -0,0 +1,16 @@
+
+function createCard(id, question, answers, correctAnswer){
+    return {
+        id: id,
+        question: question,
+        answers: answers,
+        correctAnswer: correctAnswer
+    }
+
+}
+
+
+
+module.exports = {
+    createCard,
+}
